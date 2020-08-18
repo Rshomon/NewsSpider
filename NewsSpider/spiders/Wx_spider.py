6a53fb4b7b7700dcd33ed3fb8e72ed443e7231ef
Rashomon <793652230@qq.com>
2020-08-18 18:18:15 +0800

    Test universal spider

--- NewsSpider/spiders/Wx_spider.py
+++ NewsSpider/spiders/Wx_spider.py
@@ -32,7 +32,7 @@ class WxspiderSpider(scrapy.Spider):
             '_sug_': 'n',
         }
         url = start_urls + urlencode(param)
-        yield scrapy.Request(url=url, callback=self.parsea,meta={"isProxy":True})
+        yield scrapy.Request(url=url, callback=self.parsea,meta={"isProxy":False,"download_timeout":2})
 
     '''
     获取列表中的@href，然后交由回掉处理
@@ -60,7 +60,7 @@ class WxspiderSpider(scrapy.Spider):
             # 获取当前页面的下一页地址，重新解析
             yield scrapy.Request(url=site_url,
                                  callback=self.parsea,
-                                 headers=headers,meta={"isProxy":True})
+                                 headers=headers,meta={"isProxy":False})
 
         # 目标网址
         response_url = response.xpath(
@@ -75,7 +75,7 @@ class WxspiderSpider(scrapy.Spider):
 
             yield scrapy.Request(url=url,
                                  callback=self.analysis_url,
-                                 meta={"time": t,"isProxy":True},
+                                 meta={"time": t,"isProxy":False},
                                  headers=headers)
 
     def analysis_url(self, response):
@@ -86,7 +86,7 @@ class WxspiderSpider(scrapy.Spider):
             print(new_urlaaaaa + "\n")
             yield scrapy.Request(new_urlaaaaa,
                                  callback=self.parse_detail,
-                                 dont_filter=True,meta={"isProxy":True})
+                                 dont_filter=True,meta={"isProxy":False})
         else:
             pass
 
